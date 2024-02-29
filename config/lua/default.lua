f41ea30c71452b4865d9437fb8c51770a8b5e132
tarnacious <tarn@tarnbarford.net>
2024-02-29 10:46:47 +0100

    Add copy to system keyboard shortcuts

--- config/lua/default.lua
+++ config/lua/default.lua
@@ -9,3 +9,12 @@ vim.opt.expandtab = true
 vim.opt.smartindent = true
 vim.opt.number = true
 vim.opt.termguicolors = true
+
+vim.api.nvim_set_keymap('v', '<leader>y', '"+y', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>Y', '"+yg', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>y', '"+y', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>yy', '"+yy', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>p', '"+p', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>P', '"+P', { noremap = true })
+vim.api.nvim_set_keymap('v', '<leader>p', '"+p', { noremap = true })
+vim.api.nvim_set_keymap('v', '<leader>P', '"+P', { noremap = true })
