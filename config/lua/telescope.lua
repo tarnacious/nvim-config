8850eb588bb3d02f3f474fc7ed1cdd71b8b67b61
tarnacious <tarn@tarnbarford.net>
2024-03-28 15:30:06 +0100

    Simplify some key bindings

--- config/lua/telescope.lua
+++ config/lua/telescope.lua
@@ -7,6 +7,6 @@ vim.keymap.set('n', '<leader>f', builtin.find_files, {})
 vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
 vim.keymap.set('n', ';', builtin.buffers, {})
 vim.keymap.set('n', '<leader>h', builtin.help_tags, {})
-vim.keymap.set('n', '<leader>gr', builtin.lsp_references, {})
-vim.keymap.set('n', '<leader>gd', builtin.diagnostics, {})
+vim.keymap.set('n', '<leader>r', builtin.lsp_references, {})
+vim.keymap.set('n', '<leader>d', builtin.diagnostics, {})
 
