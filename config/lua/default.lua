d9ec0686774228d6cb67f27596224417b8f064c2
tarn <tarn.barford@mpb.com>
2025-03-22 21:48:56 +0100

    Add a couple of helper shortcuts

--- config/lua/default.lua
+++ config/lua/default.lua
@@ -18,6 +18,12 @@ vim.api.nvim_set_keymap('n', '<leader>p', '"+p', { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>P', '"+P', { noremap = true })
 vim.api.nvim_set_keymap('v', '<leader>p', '"+p', { noremap = true })
 vim.api.nvim_set_keymap('v', '<leader>P', '"+P', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>fn', ':let @+=expand("%:p")<CR>', { noremap = true, silent = true })
+
+vim.keymap.set("n", "<leader>tf", function()
+  local filename = vim.fn.expand("%")
+  vim.cmd("split | terminal npm test " .. filename)
+end, { desc = "Run npm test on current file" })
 
 vim.opt.foldmethod = 'expr'
 vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
